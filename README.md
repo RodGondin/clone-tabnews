42026410ceacc3b22ba6f67be82b5694d585f0dd
Rodrigo Gondin Gevaerd <71518244+RodGondin@users.noreply.github.com>
2025-10-16 02:54:19 +0000

    adicionando scripts  and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto apredendo a construir o site https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Testando o deploy na vercel a fundo!</h1>
+  return <h1>Testando o deploy na vercel a fundo!</h1>;
 }
 
 export default Home;
