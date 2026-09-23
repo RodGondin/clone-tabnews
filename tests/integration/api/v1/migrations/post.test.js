42ba23f8891f81b45046b05c768ee558cce7844c
Rodrigo Gondin Gevaerd <71518244+RodGondin@users.noreply.github.com>
2026-09-23 01:09:14 +0000

    feature: adicionando mais testes no post do migrations

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,6 +6,26 @@ async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
+test("Any method other then POST and GET to /api/v1/migrations should return 405", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "PATCH",
+  });
+  expect(response1.status).toBe(405);
+
+  const response1Body = await response1.json();
+
+  expect(Array.isArray(response1Body)).toBe(false);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+  expect(response2.status).toBe(405);
+
+  const response2Body = await response2.json();
+
+  expect(Array.isArray(response2Body)).toBe(false);
+});
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
